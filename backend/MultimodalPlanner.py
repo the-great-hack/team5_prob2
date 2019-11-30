f688bb42610ce7b3af6b3af2a2af13bf984e1447
imalik8 <imalik8@gatech.edu>
2019-11-30 18:25:55 +0500

    First Commit of Backend

---
+++ backend/MultimodalPlanner.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+class Multimodal_Planner:
+
+	def __init__(self,x):
+		pass
+
+	def load_map_statespace(self,data_path):
+		pass
+
+	def load_map_scores(self,data_path):
+		pass
+
+	def get_GridCell(self,r,c):
+		pass
